901360e69c47cb8b75ef717a3f8ade06a178f971
MaskAdam <wychengpublic@163.com>
2020-11-20 10:01:57 +0800

    remove redundant files

--- TaskFlows/prepareData.py
+++
@@ -1,8 +0,0 @@
-'''
-Prepare data for training
-1. add character level spans (if needed)
-2. add token level spans
-3. generate a dict mapping id to tag
-4. generate a dict mapping id to token (for LSTM if needed)
-5. generate other files if needed
-'''
