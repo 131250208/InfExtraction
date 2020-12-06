75f7920f1a650870dff8e5354abb3c7f0f5efb8d
MaskAdam <wychengpublic@163.com>
2020-12-06 19:43:47 +0800

    time

--- InfExtraction/modules/model_components.py
+++ InfExtraction/modules/model_components.py
@@ -230,9 +230,7 @@ class InteractionKernel(nn.Module):
         batch_size, matrix_size, _, _ = rel_hs_hiddens.size()
 
         # ent_hs_hiddens_mirror: (batch_size, matrix_size, matrix_size, ent_dim)
-        t1 = time.time()
         ent_hs_hiddens_mirror = self._mirror(ent_hs_hiddens)
-        t2 = time.time()
 
         # ent_row_cont: (batch_size, ent_dim, matrix_size, 1)
         ent_row_cont = torch.matmul(ent_hs_hiddens_mirror.permute(0, 3, 1, 2), self.ent_alpha)
@@ -249,10 +247,7 @@ class InteractionKernel(nn.Module):
         # rel_context: (batch_size, matrix_size, matrix_size, rel_dim)
         rel_context = torch.matmul(rel_row_cont, rel_col_cont).permute(0, 2, 3, 1)
 
-        t3 = time.time()
         rel_context = self._drop_lower_triangle(rel_context)
-        t4 = time.time()
-        print("mirror: {:.3}, drop: {:.3}".format(t2 - t1, t4 - t3))
 
         ent_hs_hiddens_guided = self.rel_guide_ent_cln(ent_hs_hiddens, rel_context)
         
