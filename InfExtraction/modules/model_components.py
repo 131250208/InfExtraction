18fec93669027243d70e59ea5d241395d972eafe
MaskAdam <wychengpublic@163.com>
2020-12-07 11:59:13 +0800

    contiguous

--- InfExtraction/modules/model_components.py
+++ InfExtraction/modules/model_components.py
@@ -154,6 +154,7 @@ class HandshakingKernel(nn.Module):
                 upper_visible = visible.permute(0, 3, 1, 2).triu().permute(0, 2, 3, 1)
 
                 # visible4lstm: (batch_size * matrix_size, matrix_size, hidden_size)
+                upper_visible = upper_visible.contiguous()
                 visible4lstm = upper_visible.view(-1, matrix_size, vis_hidden_size)
                 span_pre, _ = self.lstm4span(visible4lstm)
                 span_pre = span_pre.view(batch_size, matrix_size, matrix_size, vis_hidden_size)
