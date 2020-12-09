5bea4040893368f856b38b38e9fed963d93ae551
MaskAdam <wychengpublic@163.com>
2020-12-09 18:13:00 +0800

    add utils

---
+++ InfExtraction/modules/utils.py
@@ -0,0 +1,46 @@
+import torch
+from InfExtraction.modules.preprocess import Indexer
+
+
+class MyMaths:
+    @staticmethod
+    def handshaking_len2matrix_size(hsk_len):
+        matrix_size = int((2 * hsk_len + 0.25) ** 0.5 - 0.5)
+        return matrix_size
+
+
+class MyMatrix:
+    @staticmethod
+    def mirror(shaking_seq):
+        batch_size, handshaking_seq_len, hidden_size = shaking_seq.size()
+
+        # if self.cached_mirror_gather_tensor is None or \
+        #         self.cached_mirror_gather_tensor.size()[0] != batch_size:
+        #     self.cached_mirror_gather_tensor = self.mirror_gather_tensor[None, :, None].repeat(batch_size, 1, hidden_size)
+
+        matrix_size = MyMaths.handshaking_len2matrix_size(handshaking_seq_len)
+        map_ = Indexer.get_matrix_idx2shaking_idx(matrix_size)
+        mirror_select_ids = [map_[i][j] if i <= j else map_[j][i] for i in range(matrix_size) for j in range(matrix_size)]
+        mirror_select_vec = torch.tensor(mirror_select_ids).to(shaking_seq.device())
+
+        # shaking_hiddens = torch.gather(shaking_seq, 1, self.cached_mirror_gather_tensor)
+
+        # matrix = shaking_hiddens.view(batch_size, self.matrix_size, self.matrix_size, hidden_size)
+
+        matrix = torch.index_select(shaking_seq, dim=1, index=mirror_select_vec)
+        matrix = matrix.view(batch_size, matrix_size, matrix_size, hidden_size)
+        return matrix
+
+    @staticmethod
+    def drop_lower_diag(ori_tensor):
+        '''
+        :param ori_tensor: (batch_size, matrix_size, matrix_size, hidden_size)
+        :return: (batch_size, matrix_size + ... + 1, hidden_size)
+        '''
+        tensor = ori_tensor.permute(0, 3, 1, 2).contiguous()
+        uppder_ones = torch.ones([tensor.size()[-1], tensor.size()[-1]]).long().triu().to(ori_tensor.device)
+        upper_diag_ids = torch.nonzero(uppder_ones.view(-1), as_tuple=False).view(-1)
+        # flat_tensor: (batch_size, matrix_size * matrix_size, hidden_size)
+        flat_tensor = tensor.view(tensor.size()[0], tensor.size()[1], -1).permute(0, 2, 1)
+        tensor_upper = torch.index_select(flat_tensor, dim=1, index=upper_diag_ids)
+        return tensor_upper
